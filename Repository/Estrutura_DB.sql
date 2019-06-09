d64bc8d0f32ac5093920170af9c2b512e5d71a37
Sergio Segaty <sergio.segaty@gmail.com>
2019-06-09 19:58:32 -0300

    Desenvolvimento Repositório
    Desenvolvimento das funções:
    
    InserirRegistro()
    ObterTodos() - // List<Jogo>

---
+++ Repository/Estrutura_DB.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE jogos (
+	
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(100),
+	genero VARCHAR(100),
+	classificacao VARCHAR(100),
+	preco DECIMAL(7,2),
+	data_lancamento DATETIME2,
+	qtd_estoque INT
+
+);
--- Repository/Repositorio.cs
+++ Repository/Repositorio.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,61 @@ namespace Repository
 {
     class Repositorio
     {
+        string CadeiaDeConexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\Seek3r_Home.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public void InserirRegistro(Jogo jogo)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaDeConexao;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"INSERT INTO jogos 
+            (nome, genero, classificacao, preco, data_lancamento, qtd_estoque)
+
+            VALUES
+            (@NOME, @GENERO, @CLASSIFICACAO, @PRECO, @DATA_LANCAMENTO, @QTD_ESTOQUE)";
+
+            comando.Parameters.AddWithValue("@NOME", jogo.Nome);
+            comando.Parameters.AddWithValue("@GENERO", jogo.Genero);
+            comando.Parameters.AddWithValue("@CLASSIFICACAO", jogo.Classificacao);
+            comando.Parameters.AddWithValue("@PRECO", jogo.Preco);
+            comando.Parameters.AddWithValue("@DATA_LANCAMENTO", jogo.DataLancamento);
+            comando.Parameters.AddWithValue("@QTD_ESTOQUE", jogo.qtdEstoque);
+
+            comando.ExecuteNonQuery();
+            conexao.Close();
+        }
+
+        public List<Jogo> ObtedTodos(int id)
+        {
+            List<Jogo> listaJogos = new List<Jogo>();
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaDeConexao;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            DataTable tabela = new DataTable();
+            comando.CommandText = @"SELECT * FROM jogos";
+            tabela.Load(comando.ExecuteReader());
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                Jogo jogo = new Jogo();
+                DataRow row = tabela.Rows[i];
+
+                jogo.ID = Convert.ToInt32(row["id"]);
+                jogo.Nome = row["nome"].ToString();
+                jogo.Preco = Convert.ToDecimal(row["preco"]);
+                jogo.Genero = row["genero"].ToString();
+                jogo.qtdEstoque = Convert.ToInt32(row["qtd_estoque"]);
+                jogo.DataLancamento = Convert.ToDateTime(row["data_lancamento"]);
+
+                listaJogos.Add(jogo);
+            }
+
+            return listaJogos;
+        }
     }
 }
